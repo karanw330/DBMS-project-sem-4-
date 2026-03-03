c62d4d7039a8934ff1468ea26786ed9766c2aef5
karanw330 <karanwadhwani2005@gmail.com>
2026-03-03 21:37:06 +0530

    fixed config

--- frontend/config.js
+++ frontend/config.js
@@ -11,17 +11,17 @@ function getUser() {
 function requireAuth(role) {
     const user = getUser();
     if (!user) {
-        window.location.href = "../index.html";
+        window.location.href = "../auth/index.html";
         return;
     }
     if (role && user.role !== role) {
         alert("Unauthorized access");
-        window.location.href = "../index.html"; // Redirect to login instead of root
+        window.location.href = "../auth/index.html"; // Redirect to login instead of root
         return;
     }
 }
 
 function logout() {
     localStorage.removeItem("user");
-    window.location.href = "../index.html";
+    window.location.href = "../auth/index.html";
 }
