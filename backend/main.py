02e625c853e67d6e608567b85ff2498c3ebbff1b
karanw330 <karanwadhwani2005@gmail.com>
2026-03-08 12:45:17 +0530

    ready for prod

--- backend/main.py
+++ backend/main.py
@@ -32,5 +32,6 @@ app.mount("/", StaticFiles(directory=frontend_path, html=True), name="frontend")
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run(app, host="0.0.0.0", port=port)
 
